c0b9cba9ff2d53b0b95304ea3239bb61a55dfb71
MuhammadTalhaKhan22784 <talhatariq0608@gmail.com>
2021-04-14 00:07:26 +0500

    dasdasddasdd

--- src/Pages/Travel.js
+++ src/Pages/Travel.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import { AiOutlineArrowRight } from 'react-icons/ai';
-import { IoIosArrowDroprightCircle } from "react-icons/io";
-import "./style/Travel.css";
+import { useHistory } from "react-router";
 import range from "../assets/Asset 2.png";
+import "./style/Travel.css";
 const Travel = () => {
+  const history = useHistory();
   return (
     <React.Fragment>
       <div className="travel_nav">
@@ -12,7 +12,7 @@ const Travel = () => {
         </div>
         <div className="tr_dropdown">
           <div className="dropdown">
-            <h2 className="drp_text text-white">DROPDOWN CITY</h2>
+            <h2 className="drp_text text-white">Destination CITY</h2>
             <button
               className="btn btn-secondary dropdown-toggle"
               type="button"
@@ -43,7 +43,7 @@ const Travel = () => {
         </div>
       </div>
       <div className="travel_container">
-        <div className="travel_hero">
+        <div className="travel_hero" onClick={history.push("#")}>
           <div className="travel_box">
             <div className="tbox_content">
               <h5>Car</h5>
@@ -62,20 +62,25 @@ const Travel = () => {
               </div>
             </div>
             <div className="tbox_cost">
-            <h5 className="fs-2 text-center">
-                COST <br /> 52$
+              <h5 className="fs-2 text-orange text-center">
+                VALET <br /> XXX
+              </h5>
+            </div>
+            <div className="tbox_cost">
+              <h5 className="fs-2 text-center">
+                TOTAL <br /> 52$
               </h5>
             </div>
           </div>
           {/* <IoIosArrowDroprightCircle className="text-white io_arrow" /> */}
-          <span className="go">
+          {/* <span className="go">
             Click To <br />
             Go
-          </span>
+          </span> */}
 
           <div className="travel_box_btn travel_btn_img1"></div>
         </div>
-        <div className="travel_hero">
+        <div className="travel_hero" onClick={history.push("#")}>
           <div className="travel_box">
             <div className="tbox_content">
               <h5>Bus</h5>
@@ -88,31 +93,31 @@ const Travel = () => {
                 </span>
               </div>
 
-              <p>
-                Your (xx car in inventory) will be valeted back to your house
-                ($xxx.xx fee
-              </p>
-
               <div className="risk_range">
                 <span>Risk</span>
                 <img className="range" src={range} alt="" />
               </div>
             </div>
             <div className="tbox_cost">
+              <h5 className="fs-2 text-orange text-center">
+                VALET <br /> XXX
+              </h5>
+            </div>
+            <div className="tbox_cost">
               <h5 className="fs-2 text-center">
-                COST <br /> 52$
+                TOTAL <br /> 52$
               </h5>
             </div>
           </div>
           {/* <IoIosArrowDroprightCircle className="text-white io_arrow" /> */}
-          <span className="go">
+          {/* <span className="go">
             Click To <br />
             Go
-          </span>
+          </span> */}
 
           <div className="travel_box_btn travel_btn_img2"></div>
         </div>
-        <div className="travel_hero">
+        <div className="travel_hero" onClick={history.push("#")}>
           <div className="travel_box">
             <div className="tbox_content">
               <h5>Fly commercial</h5>
@@ -124,26 +129,29 @@ const Travel = () => {
                   Inventory Summary: <span className="ms-3">$200</span>{" "}
                 </span>
               </div>
-              <p>
-                Your (xx car in inventory) will be valeted back to your house
-                ($xxx.xx fee)
-              </p>
+
               <div className="risk_range">
                 <span>Risk</span>
                 <img className="range" src={range} alt="" />
               </div>
             </div>
             <div className="tbox_cost">
-            <h5 className="fs-2 text-center">
-                COST <br /> 52$
+              <h5 className="fs-2 text-orange text-center">
+                VALET <br /> XXX
+              </h5>
+            </div>
+
+            <div className="tbox_cost">
+              <h5 className="fs-2 text-center">
+                TOTAL <br /> 52$
               </h5>
             </div>
           </div>
           {/* <IoIosArrowDroprightCircle className="text-white io_arrow" /> */}
-          <span className="go">
+          {/* <span className="go">
             Click To <br />
             Go
-          </span>
+          </span> */}
           <div className="travel_box_btn travel_btn_img3"></div>
         </div>
       </div>
