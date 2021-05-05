acf39d36c7c99ce4322c7e8b68408ffe3bf11683
MuhammadTalhaKhan22784 <talhatariq0608@gmail.com>
2021-05-05 09:19:44 +0500

    click done

--- src/Components/UserListItems.js
+++ src/Components/UserListItems.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 const UserListItems = () => {
+  const history = useHistory()
   return (
     <React.Fragment>
-      <div className="user_list_item">
+      <div className="user_list_item" onClick={()=>{history.push("#")}}>
         <h6>Lorem Rain</h6>
         <h6>20</h6>
       </div>
